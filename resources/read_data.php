1379b5ea73ea9ae535223320b29265e7740c348b
ContardiC <c.contardi@icloud.com>
2018-11-20 16:14:26 +0100

    save_data script

---
+++ resources/read_data.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Carlo Contardi
+ * User: carlocontardi
+ * Date: 20/11/2018
+ * Time: 16:02
+ */
+$email=$_POST['email'];
+
+$filePath="data/";
+$fileName=$email.".csv";
+
+$fp=fopen($filePath.$fileName,"r");
+if($fp==null) {
+    echo "The file associated with the specified email address could not be found";
+}else{
+    $data=fread($fp,filesize($filePath.$fileName));
+    $array=explode(";",$data);
+    for($i=0;$i<3;$i++){
+        echo $array[$i]."<br/>";
+    }
+}
